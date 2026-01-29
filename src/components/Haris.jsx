958f72f3bec01e43990cf6984625c732fe0644de
haris <harisjoiya8811@gmail.com>
2026-01-29 15:34:56 +0500

    haris data

---
+++ src/components/Haris.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Haris = () => {
+  return (
+    <div>
+      <h1>Shahzaib bashoo</h1>
+      <h2>dawood khuro</h2>
+      <h2>umariii bhi</h2>
+    </div>
+  )
+}
+
+export default Haris
