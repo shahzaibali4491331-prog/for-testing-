a542674379569c9837af1c4755688b0f6ef1ec43
MuhammadUmair0412 <mu1640160@gmail.com>
2026-01-29 15:16:36 +0500

    Umair Data

--- src/components/Umair.jsx
+++ src/components/Umair.jsx
@@ -5,6 +5,8 @@ const Umair = () => {
     <div>
       <h1>Basho</h1>
       <h2>Kharo</h2>
+      <h3>QUESTION 1: MErry walay hadsay may larki kay tisray bhai kay name kay ha? </h3>
+      <button>submit</button>
     </div>
   )
 }
